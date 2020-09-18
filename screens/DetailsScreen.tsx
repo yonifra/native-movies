673e9d8d6298ca412343543117c1605a40d1b797
Jonathan Fraimorice <jonathanfr@wix.com>
2020-09-18 15:51:01 +0300

    converted DetailsScreen to styled-components

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,56 +1,70 @@
 import * as React from 'react';
-import { StyleSheet, Text, Image } from 'react-native';
-import { View } from '../components/Themed';
 import { Movie } from '../types';
+import styled from 'styled-components';
+
+const Title = styled.Text`
+    font-size: 24px;
+    color: #708ED2;
+`;
+
+const Poster = styled.Image`
+    height: 141px;
+    flex: 1;
+    width: 94px;
+    margin: 20px;
+    flex-direction: row;
+`
+
+const Overview = styled.Text`
+    flex: 1;
+    font-size: 18px;
+    color: #FFF;
+    padding: 20px;
+`
+
+const ReleaseDate = styled.Text`
+    font-size: 14px;
+    color: #FFF;
+`
+
+const Votes = styled.Text`
+    font-size: 14px;
+    color: #FFF;
+    margin-top:20px;
+`
+
+const ComponentContainer = styled.View`
+    background-color: #272532;
+    flex: 1;
+    align-items: center;
+    justify-content: flex-start;
+`
+
+const Header = styled.View`
+    flex-direction: row;
+    background-color: transparent;
+`
+
+const InfoHeader = styled.View`
+    flex-direction: column;
+    background-color: transparent;
+    flex: 2
+`
 
 export default function DetailsScreen({ data }: { data: Movie }) {
     return (
-        <View style={styles.container}>
-            <View style={{ flexDirection: 'row', backgroundColor: 'transparent' }}>
-                <Image style={styles.poster} source={{
+        <ComponentContainer>
+            <Header>
+                <Poster source={{
                     uri: data.poster_path ? 'https://image.tmdb.org/t/p/w94_and_h141_bestv2' + data.poster_path : 'https://via.placeholder.com/150/000000/FFFFFF/?text=No poster'
                 }} />
-                <View style={{ flexDirection: 'column', backgroundColor: 'transparent', flex: 2 }}>
-                    <Text style={styles.voteString}>{data.vote_average === 0 ? 'Not rated' : data.vote_average}</Text>
-                    <Text style={styles.title}>{data.title}</Text>
-                    <Text style={styles.regularText}>{new Date(data.release_date).toLocaleDateString()}</Text>
-                </View>
-            </View>
-            <Text style={styles.overview}>{data.overview}</Text>
-        </View>
+                <InfoHeader>
+                    <Votes>{data.vote_average === 0 ? 'Not rated' : data.vote_average}</Votes>
+                    <Title>{data.title}</Title>
+                    <ReleaseDate>{new Date(data.release_date).toLocaleDateString()}</ReleaseDate>
+                </InfoHeader>
+            </Header>
+            <Overview>{data.overview}</Overview>
+        </ComponentContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#272532',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start'
-    },
-    title: {
-        fontSize: 24,
-        color: '#708ED2'
-    },
-    regularText: {
-        fontSize: 14,
-        color: '#FFF'
-    },
-    voteString: {
-        fontSize: 14,
-        color: '#FFF',
-        marginTop: 20
-    },
-    overview: {
-        fontSize: 18,
-        color: '#FFF',
-        padding: 20
-    },
-    poster: {
-        height: 141,
-        flex: 1,
-        width: 94,
-        margin: 20,
-        flexDirection: 'row'
-    },
-});
