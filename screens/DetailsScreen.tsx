20e06efff81f4fe708bd853d16f4490d7a76fbf6
Jonathan Fraimorice <jonathanfr@wix.com>
2020-09-18 16:20:33 +0300

    added genres + lodash

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import _ from 'lodash';
 import { Movie } from '../types';
 import styled from 'styled-components';
 
@@ -51,7 +52,89 @@ const InfoHeader = styled.View`
     flex: 2
 `
 
+const allGenres = [
+    {
+        "id": 28,
+        "name": "Action"
+    },
+    {
+        "id": 12,
+        "name": "Adventure"
+    },
+    {
+        "id": 16,
+        "name": "Animation"
+    },
+    {
+        "id": 35,
+        "name": "Comedy"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentary"
+    },
+    {
+        "id": 18,
+        "name": "Drama"
+    },
+    {
+        "id": 10751,
+        "name": "Family"
+    },
+    {
+        "id": 14,
+        "name": "Fantasy"
+    },
+    {
+        "id": 36,
+        "name": "History"
+    },
+    {
+        "id": 27,
+        "name": "Horror"
+    },
+    {
+        "id": 10402,
+        "name": "Music"
+    },
+    {
+        "id": 9648,
+        "name": "Mystery"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Science Fiction"
+    },
+    {
+        "id": 10770,
+        "name": "TV Movie"
+    },
+    {
+        "id": 53,
+        "name": "Thriller"
+    },
+    {
+        "id": 10752,
+        "name": "War"
+    },
+    {
+        "id": 37,
+        "name": "Western"
+    }
+]
+
 export default function DetailsScreen({ data }: { data: Movie }) {
+    const genreNames = _.map(_.filter(allGenres, genre2 => _.includes(data.genre_ids, genre2.id)), res => res.name)
+    const allGenresFormatted = _.forEach(genreNames, name => name).join(', ')
+
     return (
         <ComponentContainer>
             <Header>
@@ -62,6 +145,7 @@ export default function DetailsScreen({ data }: { data: Movie }) {
                     <Votes>{data.vote_average === 0 ? 'Not rated' : data.vote_average}</Votes>
                     <Title>{data.title}</Title>
                     <ReleaseDate>{new Date(data.release_date).toLocaleDateString()}</ReleaseDate>
+                    <ReleaseDate>{allGenresFormatted}</ReleaseDate>
                 </InfoHeader>
             </Header>
             <Overview>{data.overview}</Overview>
